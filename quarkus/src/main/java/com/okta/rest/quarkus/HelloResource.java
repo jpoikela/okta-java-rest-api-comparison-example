78d02731b3ca37ac680f6975a49f3cccd226cce9
Jani Poikela <jani.poikela@compile.fi>
2023-05-24 16:51:14 +0300

    build: upgrade to Quarkus 3.0.3.Final

--- quarkus/src/main/java/com/okta/rest/quarkus/HelloResource.java
+++ quarkus/src/main/java/com/okta/rest/quarkus/HelloResource.java
@@ -2,12 +2,12 @@ package com.okta.rest.quarkus;
 
 import io.quarkus.security.Authenticated;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 import java.security.Principal;
 
 @Path("/hello")
@@ -19,7 +19,7 @@ public class HelloResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@Context SecurityContext context) {
         Principal userPrincipal = context.getUserPrincipal();
-        return "Hello, " + userPrincipal.getName() + "!";
+        return "Hello from Quarkus, " + userPrincipal.getName() + "!";
     }
 
 }
--- quarkus/src/test/java/com/okta/rest/quarkus/NativeHelloResourceIT.java
+++ quarkus/src/test/java/com/okta/rest/quarkus/NativeHelloResourceIT.java
@@ -1,8 +1,8 @@
 package com.okta.rest.quarkus;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 public class NativeHelloResourceIT extends HelloResourceTest {
 
     // Execute the same tests but in native mode.
